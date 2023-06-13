790ba978942cced8c565989d9723cb19679da2e3
Manuel-Figueroa <figueroagarciamanuel@gmail.com>
2023-06-12 23:36:33 -0600

    display non-interactive maps

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
-
+import { AfterViewInit, Component, ElementRef, Input, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from "mapbox-gl";
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrls: ['./mini-map.component.css']
 })
-export class MiniMapComponent {
+export class MiniMapComponent implements AfterViewInit{
+  @Input()
+  lngLat?:[number,number]
 
+  @ViewChild('map')
+  public divMap?: ElementRef;
+
+  public map?: Map;
+
+  ngAfterViewInit(): void {
+    if(!this.divMap?.nativeElement) throw "Map div no found";
+    if(! this.lngLat) throw "LngLat can't be null";
+    const [lng,lat]=this.lngLat;
+    const currentLngLat =new LngLat(lng,lat);
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: currentLngLat, // starting position [lng, lat]
+      zoom: 14, // starting zoom
+      interactive:false
+      });
+
+    const marker = new Marker({
+      color:'red',
+    }).setLngLat(currentLngLat).addTo(this.map);
+  }
 }
